897c68538c66fed48d3b9273b13c727b584a5f59
hapreplit <21916669-hapreplit@users.noreply.replit.com>
2023-04-08 18:30:17 +0000

    test

--- static/js/maint.js
+++ static/js/maint.js
@@ -1,7 +1,7 @@
 // maintenance
 
 function checkMaintenance() {
-  const maintenanceDate = new Date("April 03, 2023 9:00:00 MST");
+  const maintenanceDate = new Date("April 03, 2023 9:01:00 MST");
   const currentDate = new Date();
 
   if (currentDate <= maintenanceDate) {
